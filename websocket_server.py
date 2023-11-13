05f363f7f80f34ac5a9f4df58a117966fe21bcbe
Matt Soriano <mattdsoriano@gmail.com>
2023-11-13 01:01:43 -0800

    Refactored websocket server for readability
    Refactored the entire websocket server for readability and modularity. Changed some settings to improve speech detection and processing. Added comments for easier comprehension

--- websocket_server.py
+++ websocket_server.py
@@ -5,191 +5,226 @@ import ipaddress
 import json
 import ssl
 import datetime
+import time
 import os
 import wave
 import webrtcvad
 from pathlib import Path
 
 # Constants
-WSS_PORT = 8000
+WSS_PORT = 8000  # The WebSocket server port
+CHANNEL_WIDTH = 1  # Mono audio channel
+SAMPLE_RATE = 48000  # Sample rate for audio in Hz
+AUDIO_DURATION = 3  # Duration of audio in seconds to process at once
+BYTES_PER_SAMPLE = 2  # Number of bytes per sample in the audio
+LONG_AUDIO_AMOUNT = 5  # Number of audio pieces to combine for long audio
+RECORDINGS_DIR = "recordings"  # Directory to save recordings
+MAX_SPEECH_LENGTH = SAMPLE_RATE * BYTES_PER_SAMPLE * AUDIO_DURATION  # Max length of speech to process
+MIN_SPEECH_LENGTH = SAMPLE_RATE * BYTES_PER_SAMPLE  # Minimum speech length in bytes (1 second)
+PHRASE_TIMEOUT_MS = 300  # Timeout after speech ends, in ms
+FRAME_DURATION_MS = 30  # Duration of an audio frame in ms
+FRAME_SIZE = (SAMPLE_RATE * FRAME_DURATION_MS * BYTES_PER_SAMPLE * CHANNEL_WIDTH) // 1000  # Size of an audio frame in bytes
+Path(RECORDINGS_DIR).mkdir(parents=True, exist_ok=True)  # Ensure the recordings directory exists
 
-CHANNEL_WIDTH = 1  # 1 for mono audio
-SAMPLE_RATE = 48000
-AUDIO_DURATION = 3
-BYTES_PER_SAMPLE = 2
-LONG_AUDIO_AMOUNT = 5
-RECORDINGS_DIR = "recordings"
 
 # Initialize VAD
-vad = webrtcvad.Vad(1)
-MIN_SPEECH_LENGTH = SAMPLE_RATE * BYTES_PER_SAMPLE  # 1 second of speech
-PHRASE_TIMEOUT_MS = 500  # Timeout after speech ends
-FRAME_DURATION_MS = 30
-FRAME_SIZE = (SAMPLE_RATE * FRAME_DURATION_MS *
-              BYTES_PER_SAMPLE * CHANNEL_WIDTH) // 1000
+vad = webrtcvad.Vad(1)  # Aggressiveness level is defaulted to 1
 
-# Ensure 'recordings' directory exists
-Path(RECORDINGS_DIR).mkdir(parents=True, exist_ok=True)
-
-
+# SSL context for securing WebSocket connection
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-# Specify the path to your SSL certificate and private key
 ssl_context.load_cert_chain("cloudflare-cert.pem", "cloudflare-key.pem")
 
-# Read Cloudflare IP addresses from the JSON file
+# Load the Cloudflare IP ranges to only allow connections from them
 with open('cloudflare_ips.json', 'r') as json_file:
     cloudflare_ips = json.load(json_file)["cloudflare_ips"]
-
 allowed_networks = [ipaddress.ip_network(range) for range in cloudflare_ips]
-
-connected_clients = set()  # Maintain a set of connected clients
-
-
-async def is_cloudflare_ip(ip):
-    for network in allowed_networks:
-        if ipaddress.ip_address(ip) in network:
-            return True
-    return False
+connected_clients = set()   # Keep track of connected clients
 
 
-async def save_audio(filename, audio_data):
-    # Calculate the number of frames based on the audio data length
-    num_frames = len(audio_data) // BYTES_PER_SAMPLE
+class ConnectionHandler:
+    def __init__(self):
+        self.speech_buffer = bytearray()  # Buffer to hold incoming audio data
+        self.sequence = 0  # Sequence number for file naming
+        self.audio_saved = 0  # Counter for saved audio files
+        self.combined_chunks = bytearray()  # Buffer to combine audio chunks
+        self.long_chunks = bytearray()  # Buffer to hold longer audio for transcription
+        self.speech_segment_buffer = bytearray()  # Buffer to hold the current speech segment
+        self.silence_duration_ms = 0  # Counter for the duration of silence
 
-    # Save the audio data to a file
-    file_path = os.path.join(RECORDINGS_DIR, filename)
-    with wave.open(file_path, 'wb') as wf:
-        wf.setnchannels(1)  # Assuming mono audio
-        wf.setsampwidth(BYTES_PER_SAMPLE)
-        wf.setframerate(SAMPLE_RATE)
-        wf.setnframes(num_frames)
-        wf.writeframes(audio_data)
+    # Process incoming WebSocket message
+    async def process_message(self, websocket, message):
+        # Binary message handling (audio data)
+        if isinstance(message, bytes):
+            await self.process_audio_frame(websocket, message)
+        else:
+            # Handle non-binary message (JSON)
+            print("Received message:", message)
 
-    print(f"{filename} saved.")
-    # Here you would add any additional logic for handling the audio, such as transcription.
+    # Handle incoming audio frames
+    async def process_audio_frame(self, websocket, audio_frame):
+        # Append new audio data to the speech buffer
+        self.speech_buffer.extend(audio_frame)
+        frame_number = 0
+        # Process audio frames as long as there's enough data in the buffer
+        while len(self.speech_buffer) >= FRAME_SIZE:
+            frame_number += 1
+            frame = self.speech_buffer[:FRAME_SIZE]
+            self.speech_buffer = self.speech_buffer[FRAME_SIZE:]
 
+            # Use VAD to check if the current frame contains speech
+            if vad.is_speech(frame, SAMPLE_RATE):
+                self.speech_segment_buffer.extend(frame)
+                self.silence_duration_ms = 0  # Reset silence duration when speech is detected
+            else:
+                # Handle non-speech periods
+                await self.handle_non_speech_periods(websocket)
 
-async def transcribe_audio(filename, size, ws):
-    if size == 'short':
-        flask_server_url = 'http://localhost:8001/transcribeshort'
-    else:
-        flask_server_url = 'http://localhost:8002/transcribelong'
-    payload = {
-        'audio_file_path': os.path.join(RECORDINGS_DIR, filename),
-        'audio_size': size
-    }
+    # Process non-speech periods to determine if a speech segment has ended
+    async def handle_non_speech_periods(self, websocket):
+        # If there's speech data in the buffer, increment the silence duration
+        if len(self.speech_segment_buffer) > 0:
+            self.update_silence_duration()
+            # Check if silence has exceeded the timeout or the speech is too long
+            if self.should_save_speech_segment():
+                print("Processing speech segments")
+                await self.process_speech_segment(websocket)
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(flask_server_url, json=payload) as response:
-            if response.status == 200:
+    def update_silence_duration(self):
+        self.silence_duration_ms += FRAME_DURATION_MS
+
+    def should_save_speech_segment(self):
+        return (self.silence_duration_ms >= PHRASE_TIMEOUT_MS or len(self.speech_segment_buffer) + len(self.combined_chunks) >= MAX_SPEECH_LENGTH)
+
+    # Process speech segments when buffer reaches required length
+    async def process_speech_segment(self, websocket):
+        self.combined_chunks.extend(self.speech_segment_buffer)
+        self.speech_segment_buffer = bytearray()   # Clear speech segment buffer
+        self.silence_duration_ms = 0  # Reset silence duration
+
+        # Check if we have enough audio to save and transcribe
+        if len(self.combined_chunks) >= MIN_SPEECH_LENGTH:
+            # Construct filename and save audio
+            filename = f"audio_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}_{self.sequence}.wav"
+            await self.save_audio(filename, self.combined_chunks)
+            self.long_chunks.extend(self.combined_chunks)
+
+            # Reset combined chunks buffer
+            self.combined_chunks = bytearray()
+            self.sequence += 1
+            self.audio_saved += 1
+
+            # Transcribe the short audio segment
+            await self.transcribe_audio(filename, 'short', websocket)
+
+            # Every LONG_AUDIO_AMOUNT of audio pieces, transcribe long audio
+            if self.audio_saved % LONG_AUDIO_AMOUNT == 0:
+                filename = f"combinedaudio_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}_{self.sequence}.wav"
+                await self.save_audio(filename, self.long_chunks)
+
+                # Transcribe the long audio segment
+                await self.transcribe_audio(filename, 'long', websocket)
+
+                # Clear the long chunks buffer
+                self.long_chunks = bytearray()
+
+    # Save audio data to a .wav file
+    async def save_audio(self, filename, audio_data):
+        num_frames = len(audio_data) // BYTES_PER_SAMPLE
+        file_path = os.path.join(RECORDINGS_DIR, filename)
+        with wave.open(file_path, 'wb') as wf:
+            wf.setnchannels(CHANNEL_WIDTH)
+            wf.setsampwidth(BYTES_PER_SAMPLE)
+            wf.setframerate(SAMPLE_RATE)
+            wf.setnframes(num_frames)
+            wf.writeframes(audio_data)
+        print(f"{filename} saved.")
+
+    # Send audio data to transcription service and handle the response
+    async def transcribe_audio(self, filename, size, ws):
+        flask_server_url = f'http://localhost:800{1 if size == "short" else 2}/transcribe{size}'
+        payload = {'audio_file_path': os.path.join(
+            RECORDINGS_DIR, filename), 'audio_size': size}
+
+        # Post audio file to the transcription server
+        async with aiohttp.ClientSession() as session:
+            async with session.post(flask_server_url, json=payload) as response:
+                if response.status != 200:
+                    print("Failed to send audio to transcription server.",
+                          response.status)
+                    return
+
                 print("Audio sent for transcription")
+                # Extract transcription from response
                 transcription_data = await response.json()
                 transcription = transcription_data.get('transcription', '')
-                if transcription == '':
+
+                # If transcription is empty, no speech was detected
+                if not transcription:
                     print("Audio contains no speech")
-                else:
-                    message = {
-                        "transcript": transcription,
-                        "audio_size": size
-                    }
-                    json_message = json.dumps(message)
-                    await ws.send(json_message)
-                    print("Transcription:", transcription)
-            else:
-                print("Failed to send audio to transcription server.",
-                      response.status)
+                    return
+
+                # Send transcription back to the client
+                message = {"transcript": transcription, "audio_size": size}
+                await ws.send(json.dumps(message))
+                print("Transcription:", transcription)
+
+# Check if an IP address is within the allowed Cloudflare range
+async def is_cloudflare_ip(ip):
+    return any(ipaddress.ip_address(ip) in network for network in allowed_networks)
 
 
-async def websocket_server(websocket):
-    # Add client to set of connected clients
+async def websocket_server(websocket, path):
+    # Initialize the handler for this connection
+    handler = ConnectionHandler()
     connected_clients.add(websocket)
 
-    # Check the client's IP address
+    # Get the IP address of the client
     client_ip = websocket.remote_address[0]
+    # Only allow connections from IPs within the Cloudflare range
+    if not await is_cloudflare_ip(client_ip):
+        print(f"Rejected connection from {client_ip}")
+        return
 
-    if await is_cloudflare_ip(client_ip):
+    await websocket.send("Connected to WebSocket server")
+    print(f"{client_ip} has connected")
 
-        # Allow the connection
-        await websocket.send("Connected to WebSocket server")
-        print(f"{websocket.remote_address[0]} has connected")
+    try:
+        async for message in websocket:
+            # Handle message using the connection handler's state
+            await handler.process_message(websocket, message)
+    except websockets.exceptions.ConnectionClosed as e:
+        # Handle connection closed events
+        print("WebSocket connection closed", e)
+    finally:
+        # Remove the client from the connected set on disconnection
+        print(f"{client_ip} has disconnected")
+        connected_clients.remove(websocket)
 
-        sequence = 0
-        audio_saved = 0
-        combined_chunks = bytearray()
-        long_chunks = bytearray()
-        max_speech_length = SAMPLE_RATE * BYTES_PER_SAMPLE * AUDIO_DURATION
-        speech_segment_buffer = bytearray()
-        speech_buffer = bytearray()
-        silence_duration_ms = 0
-
-        try:
-            async for message in websocket:
-                if isinstance(message, bytes):
-
-                    speech_buffer.extend(message)
-
-                    while len(speech_buffer) >= FRAME_SIZE:
-                        vad_frame = speech_buffer[:FRAME_SIZE]
-                        speech_buffer = speech_buffer[FRAME_SIZE:]
-
-                        if vad.is_speech(vad_frame, SAMPLE_RATE):
-                            speech_segment_buffer.extend(vad_frame)
-                            silence_duration_ms = 0  # Reset silence duration when speech is detected
-                        else:
-                            if len(speech_segment_buffer) > 0:
-                                silence_duration_ms += FRAME_DURATION_MS
-                                if silence_duration_ms >= PHRASE_TIMEOUT_MS or len(speech_segment_buffer) + len(combined_chunks) >= max_speech_length:
-                                    # Save and transcribe the speech segment
-                                    combined_chunks.extend(
-                                        speech_segment_buffer)
-                                    speech_segment_buffer = bytearray()  # Reset the speech segment buffer
-                                    silence_duration_ms = 0  # Reset silence duration
-                                    # Now check if we have enough audio to save and transcribe
-                                    if len(combined_chunks) >= MIN_SPEECH_LENGTH:
-                                        filename = f"audio_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}_{sequence}.wav"
-                                        await save_audio(filename, combined_chunks)
-                                        long_chunks.extend(combined_chunks)
-
-                                        combined_chunks = bytearray()
-                                        sequence += 1
-                                        audio_saved += 1
-
-                                        await transcribe_audio(filename, 'short', websocket)
-
-                                        if audio_saved % LONG_AUDIO_AMOUNT == 0:
-                                            filename = f"combinedaudio_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}_{sequence}.wav"
-                                            await save_audio(filename, long_chunks)
-
-                                            await transcribe_audio(filename, 'long', websocket)
-
-                                            long_chunks = bytearray()
-                else:
-                    # Handle non-binary message (e.g., JSON)
-                    print("Received message:", message)
-        except websockets.exceptions.ConnectionClosed as e:
-            print("WebSocket connection closed", e)
 
 if __name__ == '__main__':
-    # Start the WebSocket server
-    start_server = websockets.serve(
-        websocket_server, '0.0.0.0', WSS_PORT, ssl=ssl_context)
-
-    print(f"Server is running on port {WSS_PORT}")
-
     loop = asyncio.get_event_loop()
+    # Start the server and await the server to start properly
+    start_server = websockets.serve(websocket_server, '0.0.0.0', WSS_PORT, ssl=ssl_context)
+    print(f"Server is running on port {WSS_PORT}")
+    server = loop.run_until_complete(start_server)
+
     try:
-        loop.run_until_complete(start_server)
+        # Run the event loop forever until interrupted
         loop.run_forever()
     except KeyboardInterrupt:
-        # Handle any cleanup here before stopping the program
         print("Server is shutting down.")
     finally:
-        # Cancel all future and pending tasks
-        tasks = asyncio.all_tasks(loop)
-        for task in tasks:
+        # Stop server and wait until it is closed
+        server.close()
+        loop.run_until_complete(server.wait_closed())
+
+        # Gather all pending tasks and cancel them
+        pending = asyncio.all_tasks(loop)
+        for task in pending:
             task.cancel()
-        # Gather all tasks and stop the loop
-        loop.run_until_complete(asyncio.gather(*tasks, return_exceptions=True))
-        loop.stop()
+        # Wait until all tasks are cancelled.
+        loop.run_until_complete(asyncio.gather(*pending, return_exceptions=True))
+
+        # Finally close the event loop
         loop.close()
-        print("Server has shut down successfully.")
+        print("Server has shutdown successfully")
